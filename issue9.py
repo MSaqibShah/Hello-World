5fdc1a2928cddc92db1b1b9322e33ac6dff72296
Saqib <msaqibshah001@gmail.com>
2020-04-03 15:12:40 +0530

    Create a program to log into a system using SSH and post Hello World via the wall command

---
+++ issue9.py
@@ -0,0 +1,17 @@
+import paramiko
+ip = input("ENTER IP: ")
+user = input("ENTER USERNAME: ")
+passwd = input("ENTER PASSWORD: ")
+
+client = paramiko.SSHClient()
+
+client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+client.connect(ip,username=user,password=passwd)
+
+remote_connection = client.invoke_shell()
+
+stdin, stdout, stderr = client.exec_command('wall Hello_world > /dev/null 2>&1')
+out = stdout.readlines()
+error = stderr.readlines()
+print(out)
+print(error)
